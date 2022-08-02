d5a30b0535e0a990fad5af145d3e66d7e9916c58
Petra Barus <petra.barus@kuncie.com>
2022-08-02 20:47:52 +0700

    fix rest api

--- terraform/lambda2.tf
+++ terraform/lambda2.tf
@@ -15,7 +15,6 @@ resource "aws_iam_role" "lambda2_iam_role" {
   })
 }
 
-
 resource "aws_iam_role_policy" "lambda2_iam_role_logging" {
   name = "lambda2_iam_role_logging"
   role = aws_iam_role.lambda2_iam_role.id
@@ -37,8 +36,6 @@ resource "aws_iam_role_policy" "lambda2_iam_role_logging" {
   })
 }
 
-
-
 resource "aws_lambda_function" "lambda2_func" {
   filename      = "dist/lambda2.zip"
   function_name = "lambda_test_lambda2"
@@ -50,52 +47,48 @@ resource "aws_lambda_function" "lambda2_func" {
   runtime = "go1.x"
 }
 
-resource "aws_cloudwatch_log_group" "lambda2_func_log_group" {
-  name              = "/aws/lambda/${aws_lambda_function.lambda2_func.function_name}"
-  retention_in_days = 30
+resource "aws_api_gateway_rest_api" "lambda2_api" {
+  name = "lambda2_api"
 }
 
-resource "aws_apigatewayv2_api" "lambda2_apigw" {
-  name          = "lambda2_apigw"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_resource" "lambda2_api_resource" {
+  path_part   = "resource"
+  parent_id   = aws_api_gateway_rest_api.lambda2_api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.lambda2_api.id
 }
 
-resource "aws_apigatewayv2_stage" "lambda2_apigw_stage" {
-  api_id = aws_apigatewayv2_api.lambda2_apigw.id
-
-  name        = "prod"
-  auto_deploy = true
+resource "aws_api_gateway_method" "lambda2_api_get" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda2_api.id
+  resource_id   = aws_api_gateway_resource.lambda2_api_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
 }
 
-resource "aws_apigatewayv2_integration" "lambda2_apigw_integration" {
-  api_id = aws_apigatewayv2_api.lambda2_apigw.id
-
-  integration_uri    = aws_lambda_function.lambda2_func.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
+resource "aws_api_gateway_integration" "lambda2_api_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.lambda2_api.id
+  resource_id             = aws_api_gateway_resource.lambda2_api_resource.id
+  http_method             = aws_api_gateway_method.lambda2_api_get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda2_func.invoke_arn
 }
 
-resource "aws_apigatewayv2_route" "lambda2_apigw_route" {
-  api_id = aws_apigatewayv2_api.lambda2_apigw.id
-
-  route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda2_apigw_integration.id}"
-}
-
-
-resource "aws_lambda_permission" "lambda2_apigw_lambda_permission" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+resource "aws_lambda_permission" "lambda2_apigw_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda2_func.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_apigatewayv2_api.lambda2_apigw.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.lambda2_api.execution_arn}/*/*"
 }
 
-resource "aws_cloudwatch_log_group" "lambda2_apigw_log" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda2_apigw.name}"
+resource "aws_api_gateway_deployment" "lambda2_api_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.lambda2_api_integration,
+  ]
 
-  retention_in_days = 30
+  rest_api_id = aws_api_gateway_rest_api.lambda2_api.id
+  stage_name  = "prod"
 }
 
 output "lambda2_arn" {
@@ -103,5 +96,5 @@ output "lambda2_arn" {
 }
 
 output "lambda2_url" {
-  value = aws_apigatewayv2_stage.lambda2_apigw_stage.invoke_url
+  value = "${aws_api_gateway_deployment.lambda2_api_deployment.invoke_url}resource"
 }
